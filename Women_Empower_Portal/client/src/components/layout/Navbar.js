5a20ee7b4258e02080559b64ecf504423db2d112
“Surbhi <surbhisharma9099@gmail.com>
2023-01-08 02:25:18 -0500

    All files are updated

---
+++ Women_Empower_Portal/client/src/components/layout/Navbar.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from "react";
+import { Link,Routes,Route,Navigate } from "react-router-dom";
+
+// redux
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        
+        <Link to="/profiles"> Investor's</Link>
+      </li>
+      <li>
+        {/* <Link to="http://localhost:3000/signup">Jobs&Updates</Link>
+         */}
+         <Link to={{ pathname: "http://localhost:3000/signup" }} target="_blank" >Jobs&Updates</Link>
+
+         
+{/* <Route path='/privacy-policy' component={() => {
+    window.location.href = 'https://example.com/1234';
+    return null;
+}}/> */}
+
+      </li>
+      <li>
+        <Link to="/posts">Posts</Link>
+       </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Investors</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i>Cbuilds
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
---
+++ Women_Empower_Portal/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const config = require("config");
+const db = config.get('mongoURI');
+
+//gives back a promise
+
+
+
+const connectDB = async () => {
+    try {
+        //returns a promise
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true, 
+            useFindAndModify: false
+        });
+
+        console.log('mongodb connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
